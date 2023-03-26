23442bbbf91662b2dbb365e8981309f77d02a7e0
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2023-03-26 10:31:45 +0900

    [add] 必要なフォルダの追加、シーン遷移機能の作成

---
+++ Assets/Scripts/OutGame/SceneLoader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーン遷移のクラス
+/// </summary>
+public class SceneLoader : MonoBehaviour
+{
+
+
+
+    /// <summary>
+    /// 名前を指定してシーンをロードする
+    /// </summary>
+    /// <param name="sceneName">ロードするシーンの名前</param>
+    public void SceneLoad(string sceneName)
+    {
+        Debug.Log($"Loading {sceneName}");
+        SceneManager.LoadScene(sceneName);
+    }
+}
