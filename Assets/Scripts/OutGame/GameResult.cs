be0f69a452380dd883afc6b798d0610f677dc426
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2023-03-26 18:41:18 +0900

    [update] ゲームリザルト機能の作成中

---
+++ Assets/Scripts/OutGame/GameResult.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// ゲームリザルトのクラス
+/// </summary>
+[RequireComponent(typeof(CanvasGroup))]
+public class GameResult : MonoBehaviour
+{
+    [SerializeField, Tooltip("表示させるパネル")] Image _image;
+    CanvasGroup _canvasGroup;
+
+    private void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _canvasGroup.alpha = 0f;
+        _canvasGroup.blocksRaycasts = false;
+    }
+
+    public void ShowResult(float endValue = 1, float duration = 1f)
+    {
+        _canvasGroup.alpha = 1f;
+        _canvasGroup.blocksRaycasts = true;
+        _image.DOFade(endValue, duration);      
+    }
+}
